30b19e0074fb2a7d2fde96b2f6ec8c403ce3401c
AnataArisa <92080339+AnataAria@users.noreply.github.com>
2025-02-18 00:07:48 +0700

    add handle key input

--- smartdoor-esp32-client/src/main.cpp
+++ smartdoor-esp32-client/src/main.cpp
@@ -1,14 +1,30 @@
 #include <Arduino.h>
+#include <Keypad.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
 const int LCD_SDA_PIN = 21;
 const int LCD_SCL_PIN = 22;
 
+const byte KEYPAD_ROWS = 4;
+const byte KEYPAD_COLS = 4;
+
+char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte rowPins[KEYPAD_ROWS] = {2, 3, 4, 5};
+byte colPins[KEYPAD_COLS] = {6, 7, 8, 9};
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
+String keyboardEnter = "";
 LiquidCrystal_I2C lcd(0x00,16,2);
 
 byte scan_i2c_address();
 void setup_lcd_device ();
+void handle_keypad_input();
 
 void setup() {
   Serial.begin(9600);
@@ -16,6 +32,7 @@ void setup() {
 }
 
 void loop() {
+  handle_keypad_input();
 }
 
 void setup_lcd_device () {
@@ -76,4 +93,29 @@ byte scan_i2c_address() {
     Serial.println("done");
   }
   return 0;
+}
+
+void handle_keypad_input() {
+  char key = keypad.getKey();
+  if (key) {
+    Serial.print("Key pressed: ");
+    Serial.println(key);
+    if (key == '#') {
+      Serial.print("You entered: ");
+      Serial.println(keyboardEnter);
+      print_to_lcd(0, "You entered:");
+      print_to_lcd(1, keyboardEnter);
+      keyboardEnter = "";
+    } else if (key == '*') {
+      keyboardEnter = "";
+      print_to_lcd(0, "Enter Your Password");
+      print_to_lcd(1, "");
+    } else {
+      keyboardEnter += key;
+      print_to_lcd(1, keyboardEnter);
+      if (keyboardEnter.length() > 16) {
+        keyboardEnter = keyboardEnter.substring(1);
+      }
+    }
+  }
 }
