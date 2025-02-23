ac5af1da314670fc0c2b75f07385b8cf016e0e9b
AnataArisa <92080339+AnataAria@users.noreply.github.com>
2025-02-24 04:34:12 +0700

    update deploy and adding method for arduino event call

--- smartdoor-arduino-client/src/main.cpp
+++ smartdoor-arduino-client/src/main.cpp
@@ -2,10 +2,16 @@
 #include "utils/keypad_utils.h"
 #include "utils/rfid_utils.h"
 #include "utils/serial_utils.h"
+#include "events/event_producer.h"
+#include "events/input_mode.h"
+
+InputMode currentMode = InputMode::NONE;
+
 void setup() {
   Serial.begin(9600);
   setup_rfid();
   setup_software_serial();
+  trigger_event(EventType::PASSWORD_LOGIN, "");
 }
 
 void loop() {
